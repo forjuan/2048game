408d3e1165be9035380a95f6a13a00afbf279262
LJ <1044110682@qq.com>
2015-11-28 21:10:46 +0800

    fixed bug

--- js/2048.js
+++ js/2048.js
@@ -5,6 +5,7 @@ var loca = new Array(4);//位置
     }
 var box = div.getElementsByTagName("div");//每个div
 var playOff=false;//是否开始游戏
+
 function coordilate(){
 	var width = box[0].offsetWidth;
 	var height = box[0].offsetHeight;
@@ -26,18 +27,20 @@ function coordilate(){
 	}
 };
 function radom(off) {//随机产生一个空位置
-       if(!off) { return false;}
+      if(!off) {
+        return false;
+        }
     	var radomvalue = 2*Math.floor(Math.random()*2+1);//随机值
     	var empty = [];
     	var count = 0;
-    	for(var i=0;i<box.length;i++){//空的数量
+      for(var i=0;i<box.length;i++){//空的数量
     	  if(box[i].value == 0){
              count++;
              box[i].count = count;
              box[i].change = false;//是否被更改过
     	  }
-    	}
-     var radomlo = Math.floor(Math.random()*count+1);//随机出现的空位置
+     }
+      var radomlo = Math.floor(Math.random()*count+1);//随机出现的空位置
     	for(i=0;i<box.length;i++){//随机出现的那个空位置的内容为随机值
               if( box[i].count == radomlo){
                  box[i].value = radomvalue;
@@ -75,261 +78,200 @@ function color(obj){//颜色值
         obj.style.background = '#CCC0B2';
      }
 };
-function moveLeft(){
-    var move = 0;
-    var left =0,top =0;
+function move(dir){
+  var move = 0;
+  if(dir == 'left' || dir == 'top'){
     for(var i = 0;i<4;i++){
       for(var j = 0;j< 4;j++){
-        if(loca[i][j].value){
-            var tarcol = findtargetL(i,j);
-            if(j == tarcol) continue;
-            var now = findindex(i,j);
-            var pre = findindex(i,tarcol);
+       if(dir == 'left'){//左边
+          if(loca[i][j].value){
+              var tarcol = findtarget(i,j,"left");
+              if(j == tarcol) continue;
+              var now = findindex(i,j);
+              var pre = findindex(i,tarcol);
+              changeData(now,pre,tarcol,i,j,"left");
+          }
+       }else if(dir == 'top'){//向上移动
+              if(loca[j][i].value){
+                var tarcol = findtarget(j,i,"top");
+                if (j == tarcol) continue;
+                var now = findindex(j,i);
+                var pre = findindex(tarcol,i);
+                changeData(now,pre,tarcol,i,j,"top");
+             
+              }
+      }
+         move++;
+   }
+   }
+  }else if(dir == 'down' || dir == "right"){
+     for(var i = 0;i<4;i++){//i行
+      for(var j = 3;j>=0;j--){//j列
+         if(dir == 'right'){//右移动
+            if(loca[i][j].value){
+              var tarcol = findtarget(i,j,"right");
+              if(tarcol == j) {continue;}
+              var now = findindex(i,j);
+              var pre = findindex(i,tarcol);
+              changeData(now,pre,tarcol,i,j,"right");
+              move++;
+            }
+        }else if(dir == 'down'){//下移动
+            if(loca[j][i].value){
+            var tarcol = findtarget(j,i,"down");
+            if(tarcol == j) {continue;}
+            var now = findindex(j,i);
+            var pre = findindex(tarcol,i);
+            changeData(now,pre,tarcol,i,j,"down");
+            move++;
+            box[now].change = true;
+        }
+        }
+      }
+         move++;
+    }
+  }
+  for(i=0;i<box.length;i++){
+       box[i].change = false;
+        if(box[i].value <=512 ){
+          box[i].style.fontSize = "60px";
+       }
+    }
+    if(move++){//有移动才产生新值
+        radom(playOff);
+    }
+}
+function findtarget(m,n,dir){
+    var col= undefined;
+    if(dir == "down"){
+        for(var j = m;j<4;j++){//最you边的空值 
+           if(loca[j][n].value && j!=m){//从左往右第一个有值的位置
+              col=j;//找到第一个就退出循环
+              break;
+           }
+        }
+        if(col == undefined) {//该行最右边列位置是目标位置
+            return 3;
+        }else {
+              var now = findindex(m,n);
+              var rep = findindex(col,n);
+             if (box[now].value == box[rep].value && !box[rep].change){//若值相等并且被替换的div没有被改变过值
+              changevalue(now,pre);
+               return col; 
+             }else {//不相等
+              return col-1;
+            }
+       }
+    }else if(dir == "left"){
+         for(var j = n;j>=0;j--){//最左边的空值 min ：有几个空位
+           if(loca[m][j].value && j!=n){//从左往右第一个有值的位置
+              col=j;
+              break;
+           }
+         }
+         if(col == undefined) {//该行最左边列位置是目标位置
+             return 0;
+         }else {
+            var now = findindex(m,n);
+            var rep = findindex(m,col);
+           if (box[now].value == box[rep].value && !box[rep].change){//若值相等并且被替换的div没有被改变过值
+              changevalue(now,rep);
+              return col; 
+           }else {//不相等
+             return col+1;
+          }
+    }
+    }else if(dir == "top"){
+        for(var j = m;j>=0;j--){//最top边的空值 
+               if(loca[j][n].value && j!=m){//从左往右第一个有值的位置
+                  col=j;//找到第一个就退出循环
+                  break;
+               }
+        }
+        if(col == undefined) {//该行最上边列位置是目标位置
+              return 0;
+        }else {
+            var now = findindex(m,n);
+            var rep = findindex(col,n);
+         if (box[now].value == box[rep].value && !box[rep].change){//若值相等并且被替换的div没有被改变过值
+            changevalue(now,rep);
+               return col; 
+            }else {//不相等
+               return col+1;
+            }
+       }
+    }else if(dir == "right"){
+       for(var j = n;j<4;j++){//最you边的空值 
+           if(loca[m][j].value && j!=n){//从左往右第一个有值的位置
+              col=j;//找到第一个就退出循环
+              break;
+           }
+       if(col == undefined) {//该行最右边列位置是目标位置
+            return 3;
+        }else{
+              var now = findindex(m,n);
+              var rep = findindex(col,n);
+             if (box[now].value == box[rep].value && !box[rep].change){//若值相等并且被替换的div没有被改变过值
+              changevalue(now,pre);
+               return col; 
+             }else {//不相等
+              return col-1;
+            }
+       }
+     }
+    }
+};
+function changevalue(now,rep){
+                  box[now].value = 2*box[now].value;
+                  box[rep].value = 0;
+                  box[rep].innerHTML = "";
+                  color(box[rep]);
+                  if(box[now].value>512){
+                     box[now].style.fontSize = "40px";
+                  }
+};
+function changeData(now,pre,tarcol,i,j,dir){//每次移动时改变值
+     if(dir ==  "top"){
+            box[now].x = tarcol;//改变的只有hang
+            box[now].y = i;
+            box[pre].x = j;
+            box[pre].y = i;//改变对应的坐标值
+            box[pre].style.top = loca[j][i].top + 'px';//移动位置
+            box[now].style.top = loca[tarcol][i].top + "px";
+            loca[j][i].value = false;
+            loca[tarcol][i].value =true;
+     }else if(dir == 'left'){
             box[pre].style.left = loca[i][j].left + 'px';//移动位置
             box[now].style.left = loca[i][tarcol].left+"px";
             box[now].x = i;//改变的只有列
             box[now].y = tarcol;
             box[pre].x = i;
             box[pre].y = j;//改变对应的坐标值
-            box[now].innerHTML = box[now].value;
-            color(box[now]);
             loca[i][j].value = false;
             loca[i][tarcol].value =true;
-            move++;
-            box[now].change = true;
-        }
-      }
-    }
-    for(i=0;i<box.length;i++){
-       box[i].change = false;
-    }
-    if(move++){//有移动才产生新值
-       moveL = true;
-      radom(playOff);
-    }else{
-      moveL = false;
-    }
-};
-function findtargetL(m,n){
-    //找到左边第一个值为非空值
-    var col= undefined;
-    for(var j = n;j>=0;j--){//最左边的空值 min ：有几个空位
-           if(loca[m][j].value && j!=n){//从左往右第一个有值的位置
-              col=j;
-              break;
-           }
-    }
-    if(col == undefined) {//该行最左边列位置是目标位置
-          return 0;
-    }else {
-        var now = findindex(m,n);
-        var rep = findindex(m,col);
-
-        if (box[now].value == box[rep].value && !box[rep].change){
-            box[now].value = 2*box[now].value;
-            box[rep].value = 0;
-            box[rep].innerHTML = "";
-            color(box[rep]);
-           if(box[now].value>512){
-               box[now].style.fontSize = "40px";
-            }
-           return col; 
-        }else {//不相等
-           return col+1;
-        }
-    }
-};
-function findtargetT(m,n){
-    //找到上边第一个非空值
-    var row= undefined;
-    for(var j = m;j>=0;j--){//最top边的空值 
-           if(loca[j][n].value && j!=m){//从左往右第一个有值的位置
-              row=j;//找到第一个就退出循环
-              break;
-           }
-    }
-    if(row == undefined) {//该行最上边列位置是目标位置
-          return 0;
-    }else {
-        var now = findindex(m,n);
-        var rep = findindex(row,n);
-        if (box[now].value == box[rep].value && !box[rep].change){//若值相等
-            box[now].value = 2*box[now].value;
-            box[rep].value = 0;
-            box[rep].innerHTML = "";
-            color(box[rep]);
-          if(box[now].value>512){
-               box[now].style.fontSize = "40px";
-            }
-           return row; 
-        }else {//不相等
-           return row+1;
-        }
-    }
-};
-function moveTop(){
-    var move = 0;
-    var left =0,top =0;
-    for(var i = 0;i<4;i++){//i列
-      for(var j = 0;j< 4;j++){//j行
-        if(loca[j][i].value){
-            var tarcol = findtargetT(j,i);
-            if (j == tarcol) continue;
-            var now = findindex(j,i);
-            var pre = findindex(tarcol,i);
-            box[now].x = tarcol;//改变的只有hang
+     }else if(dir == 'down'){
+            box[now].style.top = loca[tarcol][i].top+"px";
+            box[pre].style.top = loca[j][i].top + 'px';//移动位置
+            box[now].x = tarcol;
             box[now].y = i;
             box[pre].x = j;
-            box[pre].y = i;//改变对应的坐标值
-            box[now].innerHTML = box[now].value;
-            box[pre].style.top = loca[j][i].top + 'px';//移动位置
-            color(box[now]);
-            loca[j][i].value = false;
-            loca[tarcol][i].value =true;
-            box[now].style.top = loca[tarcol][i].top + "px";
-            box[now].change = true;
-            move++;
-        }
-      }
-    }
-    for(i =0; i<box.length;i++){
-       box[i].change = false;
-    }
-    if(move++){//有移动才产生新值
-        moveT = true;
-        radom(playOff);
-    }else{
-        moveT = false;
-    }
-}
-
-function findtargetR(m,n){
-    //找到右边第一个非空值
-    var col= undefined;
-    for(var j = n;j<4;j++){//最you边的空值 
-           if(loca[m][j].value && j!=n){//从左往右第一个有值的位置
-              col=j;//找到第一个就退出循环
-              break;
-           }
-    }
-    if(col == undefined) {//该行最右边列位置是目标位置
-          return 3;
-    }else {
-        var now = findindex(m,n);
-        var rep = findindex(m,col);
-        if (box[now].value == box[rep].value && !box[rep].change){//若值相等
-            box[now].value = 2*box[now].value;
-            box[rep].value = 0;
-            box[rep].innerHTML = "";
-            color(box[rep]);
-          if(box[now].value>512){
-               box[now].style.fontSize = "40px";
-            }
-           return col; 
-        }else {//不相等
-           return col-1;
-        }
-    }
-}
-function moveRight(){
-    var move = 0;
-    var left =0,top =0;
-    for(var i = 0;i<4;i++){//i行
-      for(var j = 3;j>=0;j--){//j列
-        if(loca[i][j].value){
-            var tarcol = findtargetR(i,j);
-            if(tarcol == j) {continue;}
-            console.log("tarcol"+tarcol);
-            var now = findindex(i,j);
-            var pre = findindex(i,tarcol);
+            box[pre].y = i;//改变对应的坐标值  
+            loca[j][i].value = false;//当前没值
+            loca[tarcol][i].value =true;//有值
+     }else if(dir == 'right'){
             box[now].style.left = loca[i][tarcol].left+"px";
+            box[pre].style.left = loca[i][j].left + 'px';
             box[now].x = i;
             box[now].y = tarcol;
             box[pre].x = i;
             box[pre].y = j;//改变对应的坐标值
-            box[now].innerHTML = box[now].value;
-            box[pre].style.left = loca[i][j].left + 'px';//移动位置
-            color(box[now]);
             loca[i][j].value = false;//当前没值
             loca[i][tarcol].value =true;//有值
-            move++;
-            box[now].change = true;
-        }
-      }
-    }
-    for(i=0;i<box.length;i++){
-       box[i].change = false;
-    }
-    if(move++){//有移动才产生新值
-        moveR = true;
-        radom(playOff);
-    }else{
-       moveR = false;
-    }
-}
-function findtargetD(m,n){
-    //找到右边第一个非空值
-    var col= undefined;
-    for(var j = m;j<4;j++){//最you边的空值 
-           if(loca[j][n].value && j!=m){//从左往右第一个有值的位置
-              col=j;//找到第一个就退出循环
-              break;
-           }
-    }
-    if(col == undefined) {//该行最右边列位置是目标位置
-          return 3;
-    }else {
-        var now = findindex(m,n);
-        var rep = findindex(col,n);
-        if (box[now].value == box[rep].value && !box[rep].change){//若值相等
-            box[now].value = 2*box[now].value;
-            box[rep].value = 0;
-            box[rep].innerHTML = "";
-            color(box[rep]);
-            if(box[now].value>512){
-               box[now].style.fontSize = "40px";
-            }
-           return col; 
-        }else {//不相等
-           return col-1;
-        }
-    }
-};
-function moveDown(){
-    var move = 0;
-    for(var i = 0;i<4;i++){//i列
-      for(var j = 3;j>=0;j--){//j行
-        if(loca[j][i].value){
-            var tarcol = findtargetD(j,i);
-            console.log("tarcol"+tarcol);
-            if(tarcol == j) {continue;}
-            var now = findindex(j,i);
-            var pre = findindex(tarcol,i);
-            box[now].style.top = loca[tarcol][i].top+"px";
-            box[now].x = tarcol;
-            box[now].y = i;
-            box[pre].x = j;
-            box[pre].y = i;//改变对应的坐标值
+     }
             box[now].innerHTML = box[now].value;
-            box[pre].style.top = loca[j][i].top + 'px';//移动位置
             color(box[now]);
-            loca[j][i].value = false;//当前没值
-            loca[tarcol][i].value =true;//有值
-            move++;
             box[now].change = true;
-        }
-      }
-    }
-    for(i=0;i<box.length;i++){
-       box[i].change = false;
-    }
-    if(move++){//有移动才产生新值
-        moveD = true;
-        radom(playOff);
-    }else {
-        moveD = false;
-    }
-}
+};
 function findindex(m,n){//(m,n)坐标对应的box 需要更严谨的方法
 	for(var i=0;i<box.length;i++){
 	   if(box[i].x == m && box[i].y == n){
@@ -405,34 +347,34 @@ window.onload = function(){
 	  this.style.background = '#ccc';
 	};
 	left.onclick = function (){
-	  moveLeft();
+	  move("left");
     gameover();
 	};
   top.onclick = function (){
-    moveTop();
+    move("top");
     gameover();
   };
   right.onclick = function (){
-    moveRight();
+    move("right");
     gameover();
   };
   bottom.onclick = function (){
-    moveDown();
+    move("down");
     gameover();
   };
   EventUtil.addHandler(document,"keyup",function(event){
        event = EventUtil.getEvent(event);
        if(event.keyCode == 37){
-            moveLeft();
+            move("left");
             gameover();
        }else if(event.keyCode == 38){
-           moveTop();
+           move("top");
            gameover();
        }else if(event.keyCode == 39){
-           moveRight();
+           move("right");
            gameover();
        }else if(event.keyCode == 40){
-           moveDown();
+           move("down");
            gameover();
        }
   });
